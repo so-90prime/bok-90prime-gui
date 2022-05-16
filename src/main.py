0e021c9b3a823ad5184652919e13b63acf25ce5d
Dan Avner <ldpavner@gmail.com>
2022-05-15 23:56:12 -0700

    Added in resources and cleaned up codebase

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3.8
-import os
 from pathlib import Path
 from dotenv import dotenv_values
 
@@ -7,33 +6,37 @@ from tornado.web import StaticFileHandler
 from pyindi.webclient import INDIWebApp, INDIHandler
 
 # Load configuration
-CURRENT_DIR = Path(__file__).parent # The current directory
+CURRENT_DIR = Path(__file__).parent  # The current directory
 config = dotenv_values(CURRENT_DIR.parent / ".env")
 
 # Configuration
-WEBPORT = int(config["WEBPORT"]) # The port for the web app
-INDIPORT = int(config["INDIPORT"]) # The indiserver port
-INDIHOST = config["INDIHOST"] # Where the indiserver is running
-DEVICES = ["*"] # All devices is called by an asterisk
+WEBPORT = int(config["WEBPORT"])  # The port for the web app
+INDIPORT = int(config["INDIPORT"])  # The indiserver port
+INDIHOST = config["INDIHOST"]  # Where the indiserver is running
+DEVICES = ["*"]  # All devices is called by an asterisk
 
-TEMPLATE = "ninety-prime.html"
+TEMPLATE = CURRENT_DIR / "ninety-prime.html"
+RESOURCES_PATH = CURRENT_DIR / "resources"
+
 
 # Build handlers with path for rendering, each path should have a handler
 class GUI(INDIHandler):
     def get(self):
         # Pass additional variables to appear in the html template
-        self.indi_render(CURRENT_DIR / TEMPLATE, devices=DEVICES)
+        self.indi_render(TEMPLATE, devices=DEVICES)
+
 
 web_app = INDIWebApp(webport=WEBPORT, indihost=INDIHOST, indiport=INDIPORT)
 
 print(f"Go to http://<server_name>:{WEBPORT}")
-print(f"If the server is on localhost go to:")
+print("If the server is on localhost go to:")
 print(f"http://localhost:{WEBPORT}/")
 
 # Attach handlers and build the application
 # For images, use tornado.web.StaticFileHandler and link the path
 web_app.build_app(
     [
-        (r"/", GUI)
+        (r"/", GUI),
+        (r"/resources/(.*)", StaticFileHandler, {"path": RESOURCES_PATH})
     ],
 )
---
+++ src/resources/config.js
@@ -0,0 +1,11 @@
+const Tips = {
+  "GALIL-DMC-2280__IFOCUS_LVDT": `Controls the focus in LVDT units. Users can move the focus individually (A, B, and C) or move them
+  all with \"GOTO A, B, C\". To move the focus to one set value, use \"Delta Focus All\".`,
+  "GALIL-DMC-2280__FW_LIGHTS": `Light indicators for the current state of the filter wheel.`,
+  "GALIL-DMC-2280__IFILTER_CHANGE": `Common filter operations. Current filter selected is highlighted. Start the night with "READ FILTERS" and end
+  the night with "END OF NIGHT"`,
+  "GALIL-DMC-2280__GFOCUS_DIST": `Changes the guider stage focus in relative units.`,
+  "GALIL-DMC-2280__GW_LIGHTS": `Status light of the guider filter wheel.`,
+  "GALIL-DMC-2280__GFILTER_CHANGE": `Common guider filter wheel operations. Current filter selected is highlighted. Start the night by initializing.`,
+  "GALIL-DMC-2280__TELEMETRY": `Status lights for the galil.`,
+};
